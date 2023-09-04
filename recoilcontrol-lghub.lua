1d03b10c874399229447ca75a3bd4b3c71d9bf89
ADE YOGA WIYADNYANA <adeyogawiyadnyana@stikom-bali.ac.id>
2023-09-04 15:27:57 +0800

    Update recoilcontrol-lghub.lua

--- recoilcontrol-lghub.lua
+++ recoilcontrol-lghub.lua
@@ -1,8 +1,9 @@
 -- Universal_Simplified for Logitech GHUB
 -- github @yorisofficial
 
-local NR = 1                     -- rubah angka 1 - 5 untuk menyesuaikan weapons recoil
-local Sleep_ = 10                -- jangan rubah apapun!
+local NR = 1                            -- rubah angka 1 - 5 untuk menyesuaikan weapons recoil
+local Sleep_ = 10                       -- jangan rubah apapun!
+local ActivationButton = "scrolllock"   -- rubah untuk menyesuaikan
 
 EnablePrimaryMouseButtonEvents(true)
 
@@ -14,7 +15,7 @@ function NoRecoil()
 end
 
 function OnEvent(event, arg)
-    if IsKeyLockOn("scrolllock") and event == "MOUSE_BUTTON_PRESSED" and arg == 1 then
+    if IsKeyLockOn(ActivationButton) and event == "MOUSE_BUTTON_PRESSED" and arg == 1 then
         NoRecoil()
     end
 end
