2b11b5b33014e989b9a4bf05964f2f29beb4b914
ADE YOGA WIYADNYANA <85540115+yorisofficial@users.noreply.github.com>
2023-09-04 14:59:07 +0800

    Add files via upload

---
+++ recoilcontrol-lghub.lua
@@ -0,0 +1,20 @@
+-- Universal_Simplified for Logitech GHUB
+-- github @yorisofficial
+
+local NR = 1                     -- rubah angka 1 - 5 untuk menyesuaikan weapons recoil
+local Sleep_ = 10                -- jangan rubah apapun!
+
+EnablePrimaryMouseButtonEvents(true)
+
+function NoRecoil()
+    repeat
+        MoveMouseRelative(0, NR)
+        Sleep(Sleep_)
+    until not IsMouseButtonPressed(1)
+end
+
+function OnEvent(event, arg)
+    if IsKeyLockOn("scrolllock") and event == "MOUSE_BUTTON_PRESSED" and arg == 1 then
+        NoRecoil()
+    end
+end
